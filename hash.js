df5390ea816ae590d2f931c0dab6aadf86fe9236
Jahid Rana <jahidrana2020@gmail.com>
2025-05-21 21:26:50 +0600

    Delete hash.js

--- hash.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt');
-
-(async () => {
-    const plainPassword = 'admin12345'; // Your plaintext password
-    const hashedPassword = await bcrypt.hash(plainPassword, 10);
-   
-})();
